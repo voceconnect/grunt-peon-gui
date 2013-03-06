c7bf36a38b7e52f5fcc8ba7085f9bce52ea44418
Mark Parolisi <mark@voceconnect.com>
2013-03-06 17:02:12 -0500

    working out UI

--- app/js/peon-gui.js
+++ app/js/peon-gui.js
@@ -12,9 +12,12 @@ var PeonGUI = (function () {
             tasks: $("#tasks"),
             regularTasks: $("#placeTasks"),
             projects: $("#placeProjects"),
+            taskInfo: $("#taskInfo"),
+            runTask: $("#runTask"),
+            clearConsole : $("#clearConsole"),
             progress: $("running")
         },
-        startPort = 61750,
+        startPort = 61749,
         currentPort = startPort,
         maxPort = currentPort + 5,
         projects = [],
@@ -35,14 +38,20 @@ var PeonGUI = (function () {
 
     function updateProjectList() {
         var html = "";
-        _.each(projects, function(project, i){
+        _.each(projects, function (project, i) {
             html = html + '<li><a href="#" data-id="' + i + '">' + project.name + '</a></li>';
         });
         $html.projects.html(html);
     }
 
+    function updateTaskInfo(task) {
+        var taskObject = currentProject.tasks[task],
+            html = "<h2><%= name %></h2><p><%= info %></p><button class='btn btn-success' id='runTask' data-name='<%= name %>'>Run</button>&nbsp;<button id='killTask' class='btn btn-danger'>Stop</button>";
+        $html.taskInfo.html(_.template(html, taskObject));
+    }
+
     function setProject(idx) {
-        if (!running) {
+        if (running === false) {
             var projectEls, taskListTpl;
             currentProject = projects[idx];
             projectEls = $html.projects.find('a');
@@ -50,7 +59,7 @@ var PeonGUI = (function () {
             $(projectEls.get(idx)).parent("li").addClass('active');
             $html.output.html('');
             if (currentProject) {
-                taskListTpl = "<% _.each(projectEls, function(name) { %> <li><a href='#' data-task='<%= name %>'><%= name %></a></li><% }); %>";
+                taskListTpl = "<% _.each(projectEls, function(task) { %> <li><a href='#' data-task='<%= task.name %>'><%= task.name %></a></li><% }); %>";
                 $html.regularTasks.html(_.template(taskListTpl, {projectEls: currentProject.tasks}));
             }
         }
@@ -78,74 +87,80 @@ var PeonGUI = (function () {
             }
             if (event.data.indexOf('Running Task:') === 0) {
                 $html.output.html('');
-            } else if (event.data.length > 1) {
+            } else if (data.length > 1 && data.action != 'connected') {
                 $html.output.append("<p>" + new Date().toString().split(' ')[4] + ' - ' + event.data + "</p>");
             }
         }
 
     }
 
-function handleSocketClose(e) {
-    var closedPort = parseInt(e.currentTarget.URL.split(':')[2].replace(/\D/g, ''), 10),
-        newProjects = _.reject(projects, function (el) {
-            return el.port === closedPort;
-        });
+    function handleSocketClose(e) {
+        var closedPort = parseInt(e.currentTarget.URL.split(':')[2].replace(/\D/g, ''), 10),
+            newProjects = _.reject(projects, function (el) {
+                return el.port === closedPort;
+            });
 
-    if (newProjects.length !== projects.length) {
-        if (closedPort === currentProject.port && running) {
-            enableActivity();
+        if (newProjects.length !== projects.length) {
+            if (closedPort === currentProject.port && running) {
+                enableActivity();
+            }
+
+            projects = newProjects;
+            updateProjectList();
+            setProject(projects.length - 1);
+        } else {
+            projects = newProjects;
         }
-
-        projects = newProjects;
-        updateProjectList();
-        setProject(projects.length - 1);
-    } else {
-        projects = newProjects;
     }
-}
 
-function handleSocketError() {
-    alert('Something went really wrong, please report this...');
-}
+    function handleSocketError(error) {
+        console.log(error);
+    }
 
-function connect(port) {
-    var socketAddr,
-        exists = _.find(projects, function (project) {
+    function connect() {
+        var exists = _.find(projects, function (project) {
             return project.port === currentPort;
         });
-    if (!exists) {
-        socketAddr = 'ws://localhost:' + currentPort;
-        if (port) {
-            socketAddr = 'ws://localhost:' + port;
+
+        // if no project on that port
+        if (!exists) {
+            socket = new WebSocket('ws://localhost:' + currentPort, 'echo-protocol');
+            socket.onopen = handleSocketOpen;
+            socket.onmessage = handleSocketMessage;
+            socket.onclose = handleSocketClose;
+            socket.onerror = handleSocketError;
         }
-        socket = new WebSocket(socketAddr, 'echo-protocol');
-        socket.onopen = handleSocketOpen;
-        socket.onmessage = handleSocketMessage;
-        socket.onclose = handleSocketClose;
-        socket.onerror = handleSocketError;
+
+        if (maxPort === currentPort) {
+            currentPort = startPort;
+        }
+        currentPort++;
+        setTimeout(connect, 1000);
     }
-    if (maxPort === currentPort) {
-        currentPort = startPort;
-    }
-    currentPort++;
-    setTimeout(connect, 1000);
-}
 
-return function () {
-    connect();
-    $html.tasks.on('click', 'a', function () {
-        currentProject.socket.send($(this).data('task'));
-        $html.progress.show();
-        currentTask = {name: $(this).data('task'), output: ''};
-        disableActivity();
-    });
+    return function () {
+        connect();
+        $html.tasks.on('click', 'a', function () {
+            var taskName = $(this).data('task');
+            currentTask = {name: taskName, output: ''};
+            updateTaskInfo(taskName);
+        });
 
-    $html.projects.on('click', 'a', function () {
-        setProject($(this).data('task'));
-    });
-};
+        $html.runTask.on('click', 'a', function () {
+            var taskName = $(this).data('task');
+            currentTask = {name: taskName, output: ''};
+            currentProject.socket.send(taskName);
+            $html.progress.show();
+            disableActivity();
+        });
 
-}
-()
-)
-;
+        $html.clearConsole.click(function () {
+            $html.output.html('');
+        });
+
+        $html.projects.on('click', 'a', function () {
+            setProject($(this).data('task'));
+        });
+    };
+
+}());
---
+++ tasks/peon-servers.coffee
@@ -0,0 +1,105 @@
+module.exports = (grunt) ->
+  grunt.registerTask('gui', 'Launch a GUI web interface', () ->
+    @async()
+
+    class PeonWebSocket
+      spawn: require("child_process").spawn
+      pkg: require(process.cwd() + '/package.json')
+      workers: []
+      tasks: grunt.task._tasks
+      projectPort: 61750
+      server: require('http').createServer((request, response) ->
+        response.writeHead(404)
+        response.end()
+      )
+
+      constructor: () ->
+        process.on("uncaughtException", @killWorkers)
+        process.on("SIGINT", @killWorkers)
+        process.on("SIGTERM", @killWorkers)
+
+      killWorkers: () ->
+        @workers.forEach((worker) ->
+          process.kill(worker)
+        )
+        process.exit()
+
+      startWorker: ()->
+        ps = require('portscanner')
+        pPort = @projectPort
+        nPort = @projectPort + 4
+        that = @
+        ps.findAPortNotInUse(pPort, nPort, 'localhost', (err, port) ->
+          @projectPort = port
+          if @projectPort
+            that.server.listen(port, () ->
+              grunt.log.writeln("WebSocket running on localhost:#{port}")
+            )
+          else
+            grunt.log.writeln("Too many WebSockets open. Close one.")
+        )
+        @listen()
+
+      listen: () ->
+        WebSocketServer = require('websocket').server
+        wsServer = new WebSocketServer(
+          httpServer: @server
+          autoAcceptConnections: false
+        )
+        that = @
+        wsServer.on('request', (request) ->
+          connection = request.accept('echo-protocol', request.origin)
+          connection.on('message', (message) ->
+            if message.type is 'utf8'
+              msg = message.utf8Data
+              if msg is 'connect'
+                connection.sendUTF(JSON.stringify(
+                  tasks: that.tasks
+                  project: that.pkg.name
+                  port: that.projectPort
+                  action: "connected"
+                ))
+              else if Object.keys(that.tasks).indexOf(msg) > -1
+                watcher = that.spawn('peon', [msg, '-no-color'])
+                that.workers.push(watcher)
+                connection.send("Running Task: #{msg}")
+                watcher.stdout.on('data', (data) ->
+                  if data
+                    connection.send(data.toString())
+                )
+                watcher.stdout.on('end', (data) ->
+                  if data
+                    connection.send(data.toString())
+                  connection.sendUTF(JSON.stringify({ action: 'done'}))
+                )
+                watcher.stderr.on('data', (stderr) ->
+                  if stderr
+                    connection.send(stderr.toString())
+                )
+                watcher.on('exit', (code) ->
+                )
+          )
+          connection.on('close', () ->
+
+          )
+        )
+
+    class PeonGUIServer
+      connect: require('connect')
+      path: require('path')
+      server: false
+      run: () ->
+        portscanner = require('portscanner')
+        that = @
+        portscanner.checkPortStatus(8888, 'localhost', (err, status) ->
+          if status is 'closed'
+            appPath = that.path.resolve(__dirname, '../app')
+            that.server = that.connect.createServer(that.connect.static(appPath))
+            that.server.listen(8888)
+          grunt.log.writeln "GUI running on localhost::8888"
+        )
+
+    new PeonGUIServer().run()
+    new PeonWebSocket().startWorker()
+  )
+
